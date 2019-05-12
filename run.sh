edec4c390282503d557475c6c2b9ba80fe0b88b8
Xiao Gao <gaoxiao1988@gmail.com>
2019-05-12 02:08:46 -0700

    fix log

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 mkdir logs
 
 CUDA_VISIBLE_DEVICES=0 python gen_dataset.py --size 2000 &> logs/00.log &
-CUDA_VISIBLE_DEVICES=0 python gen_dataset.py --size 2000 &> logs/10.log &
+CUDA_VISIBLE_DEVICES=0 python gen_dataset.py --size 2000 &> logs/01.log &
 CUDA_VISIBLE_DEVICES=0 python gen_dataset.py --size 2000 &> logs/02.log &
 CUDA_VISIBLE_DEVICES=0 python gen_dataset.py --size 2000 &> logs/03.log &
 CUDA_VISIBLE_DEVICES=0 python gen_dataset.py --size 2000 &> logs/04.log &
