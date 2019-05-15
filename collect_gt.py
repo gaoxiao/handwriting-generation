b0e93e0621e5287a810cc6cd5615dc05590c86d7
Xiao Gao <gaoxiao1988@gmail.com>
2019-05-15 14:47:01 -0700

    Collect all GT.

---
+++ collect_gt.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+gt_dir = './gt'
+
+ALL_file = os.path.join(gt_dir, 'ALL')
+BACKUP_file = os.path.join(gt_dir, 'ALL_bak')
+if os.path.isfile(ALL_file):
+    shutil.copyfile(ALL_file, BACKUP_file)
+
+with open(ALL_file, 'a') as all:
+    for gt_f in os.listdir(gt_dir):
+        gt_f = os.path.join(gt_dir, gt_f)
+        if not os.path.isfile(gt_f):
+            continue
+        if not os.path.splitext(gt_f)[-1].endswith('txt'):
+            continue
+        with open(gt_f) as f:
+            for l in f:
+                all.write(l)
